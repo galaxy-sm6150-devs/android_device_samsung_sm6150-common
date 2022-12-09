3a577bec896b0abe3c6364ec47d048c9367bba4e
Alexander Koskovich <akoskovich@pm.me>
2022-12-09 20:23:05 +0100

    sm6150-common: Build the QTI AIDL power HAL from source
    Change-Id: I38229f2d21992a1de3a5449381d1aa85e434baf5

--- sm6150.mk
+++ sm6150.mk
@@ -501,6 +501,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
 
 # Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service-qti \
+
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/vendor/etc/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
 
