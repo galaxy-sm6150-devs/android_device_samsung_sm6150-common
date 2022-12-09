def81cfe2a75fe435c7bff2d7ffd21ff1a0da758
Jan Altensen <info@stricted.net>
2022-12-09 21:35:33 +0100

    sm6150-common: Build the Samsung touch service from source
    * Picked from hardware/samsung.
    
    Change-Id: Ic92c2580b33fca835f445ed4e6600a813e417441

--- sm6150.mk
+++ sm6150.mk
@@ -661,6 +661,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti \
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.samsung \
+
 # WiFi
 PRODUCT_PACKAGES += \
     WifiOverlay \
