23bc50ee1aa2b81830aec1a232569a83d4ea4235
Bruno Martins <bgcngm@gmail.com>
2022-12-10 22:48:38 +0100

    sm6150-common: Remove A2DP offload bits
    * Samsung hard disabled A2DP offload in the audio HAL.
    
    Change-Id: I1067d4ad2ee0f3d3bedea3308055bbc48650a0d4

--- sm6150.mk
+++ sm6150.mk
@@ -135,9 +135,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     audio.bluetooth.default \
-    com.qualcomm.qti.bluetooth_audio@1.0 \
-    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
-    liba2dpoffload \
     libbluetooth_qti \
     libbluetooth_audio_session \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
