fb1e8998c95c3dbfe933d489fd7cf6d98febda8d
dianlujitao <dianlujitao@lineageos.org>
2022-12-10 22:43:45 +0100

    sm6150-common: Switch to the QTI Bluetooth stack
    Change-Id: I625229fc2b9a52b97f3dbc1d52bc226b11a9d470

--- sm6150.mk
+++ sm6150.mk
@@ -137,6 +137,8 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     com.qualcomm.qti.bluetooth_audio@1.0 \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
+    liba2dpoffload \
+    libbluetooth_qti \
     libbluetooth_audio_session \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
