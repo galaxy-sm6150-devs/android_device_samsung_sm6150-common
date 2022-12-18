f15627df954c47191e8142cf040475abf118bb21
Haky86 <hakamelassouad@gmail.com>
2022-12-18 21:52:41 +0100

    Revert "sm6150-common: rootdir: Use first_stage_mount"
    * It makes fingerprint enrollment crashing under enforcing for some reason, so gonna kill for now.
    
    This reverts commit a359a6a6059fe2e065b6b733d6d2f6cdab08c349.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -25,15 +25,6 @@ LOCAL_SRC_FILES    := vendor/etc/fstab.qcom
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.qcom_ramdisk
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
-LOCAL_MODULE_STEM  := fstab.qcom
-LOCAL_MODULE_TAGS  := optional
-LOCAL_SRC_FILES    := vendor/etc/fstab.qcom
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.qcom.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
